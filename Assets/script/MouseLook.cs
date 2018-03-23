2f89c34f0c90ce51bd3c4cbbfa2b12988429b82c
frcandela <candela.francois@gmail.com>
2018-03-23 21:09:37 +0100

    squash and strech

---
+++ Assets/script/MouseLook.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+
+
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationY = 0F;
+
+    public float speed = 0.1f;
+    public float dashForce = 1f;
+
+
+    private Camera m_camera;
+    private Rigidbody m_rigidbody;
+
+    void Update()
+    {
+        if (Input.GetKey( KeyCode.Z))
+            transform.position = transform.position + speed * transform.forward;
+        if (Input.GetKey(KeyCode.S))
+            transform.position = transform.position - speed * transform.forward;
+        if (Input.GetKey(KeyCode.D))
+            transform.position = transform.position + speed * transform.right;
+        if (Input.GetKey(KeyCode.Q))
+            transform.position = transform.position - speed * transform.right;
+
+        
+
+
+        if ( Input.GetKey(KeyCode.Space) )
+        {
+            Vector2 lookAtScreen = TobiiAPI.GetGazePoint().Screen;
+            if (lookAtScreen.x >= 0 && lookAtScreen.x <= Screen.width && lookAtScreen.y >= 0 && lookAtScreen.y <= Screen.height)
+            {
+                Ray ray = m_camera.ScreenPointToRay(lookAtScreen);
+                Vector3 hitPoint = ray.GetPoint(1f) - ray.origin;
+                m_rigidbody.AddForce(dashForce * hitPoint, ForceMode.VelocityChange);
+
+            }
+        }
+
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+
+    void Awake()
+    {
+        // Make the rigid body not change rotation
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb)
+            rb.freezeRotation = true;
+
+        m_camera = GetComponent<Camera>();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+}
---
+++ Assets/script/SquachStrech.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+using UnityEngine;
+
+public class SquachStrech : MonoBehaviour
+{
+
+    [Header("RealSpeedPeerFrame = speedRatio * distance(eyePoint, cube)")]
+    public float speedRatio = 0.1f;
+
+    [Header("References")]
+    public Material moving;
+    public Material notMoving;
+    public GameObject targetObject = null;
+
+    Camera m_camera;
+
+    private void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+        TobiiAPI.SubscribeGazePointData();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        SetObjectMaterial(targetObject, moving);
+
+        Vector2 lookAtScreen = TobiiAPI.GetGazePoint().Screen;
+        if (lookAtScreen.x >= 0 && lookAtScreen.x <= Screen.width && lookAtScreen.y >= 0 && lookAtScreen.y <= Screen.height)
+        {
+            Vector3 scale = targetObject.transform.localScale;
+            float ratioX = lookAtScreen.x / Screen.width;
+            float ratioY = lookAtScreen.y / Screen.height;
+            targetObject.transform.localScale = new Vector3(ratioX, ratioY, scale.z);
+        }
+
+    }
+
+    private void SetObjectMaterial(GameObject obj, Material material)
+    {
+        MeshRenderer renderer = obj.GetComponent<MeshRenderer>();
+        if (renderer)
+            renderer.material = material;
+    }
+}
