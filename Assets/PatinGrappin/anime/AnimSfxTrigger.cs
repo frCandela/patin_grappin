9ddaf291bacc6e7f7d5f915624bd49ee6298bd84
Unknown <raoul.desmarest@gmail.com>
2018-04-27 14:45:32 +0100

    SFX integration

---
+++ Assets/PatinGrappin/anime/AnimSfxTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimSfxTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerSFX (AnimationEvent animEvent) {
+		Debug.Log("Event");
+
+		//float floatParam = animEvent.floatParameter;
+		//int intParam = animEvent.intParameter;
+		string stringParam = animEvent.stringParameter;
+
+		if(stringParam == "softContact")
+		{
+			//do
+		}
+
+		if(stringParam == "hardContact")
+		{
+			//do
+		}
+
+		if(stringParam == "softPush")
+		{
+			AkSoundEngine.PostEvent("Play_Ice_skate_move", gameObject);
+		}
+
+		if(stringParam == "hardPush")
+		{
+			//do
+		}
+
+
+	}
+}
