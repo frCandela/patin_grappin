ec4c3570dbb182b34ed04669d8d7bf19d2061183
Unknown <raoul.desmarest@gmail.com>
2018-04-28 13:49:14 +0100

    Animator
    scripts de gestion side R/L de Curve(float)  vers Bool
    Rajout pose grappin en l'air

---
+++ Assets/PatinGrappin/anime/sideCurveToBool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class sideCurveToBool : MonoBehaviour {
+
+	private Animator animator;
+	private float sideCurve;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void OnAnimatorIK () {
+
+		sideCurve = animator.GetFloat("sideCurve");
+
+		// sur le pied droit
+		if(sideCurve >= 0.5f) animator.SetBool("side", true);
+
+		// sur le pied gauche
+		else if(sideCurve < 0.5f) animator.SetBool("side", false);
+	}
+}
