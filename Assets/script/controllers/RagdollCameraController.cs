8b8bd3d350d3d1f85955b6cd325bc6bc474f33af
Candela François <candela.francois@gmail.com>
2018-05-14 17:22:50 +0200

    ragdoll camera test

---
+++ Assets/script/controllers/RagdollCameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tobii.Gaming;
+using UnityEngine;
+
+
+
+
+[RequireComponent( typeof(  Camera ))]
+public class RagdollCameraController : MonoBehaviour
+{
+    private PlayerController m_playerController;
+    private Rigidbody m_targetRb;
+    CameraController m_cameraController;
+
+    bool used = false;
+
+    private void Awake()
+    {
+        m_playerController = FindObjectOfType<PlayerController>();
+        m_targetRb = m_playerController.GetComponentInChildren<Animator>().GetBoneTransform(HumanBodyBones.Head).GetComponent<Rigidbody>();
+        m_cameraController = GetComponent<CameraController>();
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            used = true;
+            m_cameraController.enabled = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {//SetRagdoll(false);
+            used = false;
+            m_cameraController.enabled = true;
+        }
+
+        //if (false)
+        {
+            transform.position = m_targetRb.transform.position - 15 * Vector3.forward + 5 * Vector3.up;
+            transform.LookAt(m_targetRb.transform.position);
+        }
+
+    }
+
+
+
+}
---
+++ Assets/script/controllers/RagdollController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    private Animator m_animator;
+
+    private Rigidbody m_mainRb;
+    private Collider m_mainCollider;
+
+    private Rigidbody[] m_ragdollRbs;
+    private Collider[] m_ragdollColliders;
+
+    
+
+    private void Awake()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+
+        m_mainRb = GetComponent<Rigidbody>();
+        m_mainCollider = GetComponent<Collider>();
+
+        m_ragdollRbs = GetComponentsInChildren<Rigidbody>();
+        m_ragdollColliders = GetComponentsInChildren<Collider>();
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        SetRagdoll(false);
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            SetRagdoll(true);
+        if (Input.GetKeyUp(KeyCode.A))
+            SetRagdoll(false);
+    }
+
+    public void SetRagdoll(bool state)
+    {
+        if (state)//Activates ragdoll
+        {
+            //Activates ragdoll colliders and rigidbodies
+            foreach (Rigidbody rb in m_ragdollRbs)
+                if (rb != m_mainRb)
+                {
+                    rb.isKinematic = false;
+                    rb.velocity = m_mainRb.velocity;
+                }
+            foreach (Collider col in m_ragdollColliders)
+                col.enabled = true;
+
+            //Desactivate main collider, main rigidbody and animator
+            m_mainCollider.enabled = false;
+            m_mainRb.isKinematic = true;
+            m_animator.enabled = false;
+        }
+        else//Desactivate ragdoll
+        {
+            //Desactivate ragdoll colliders and rigidbodies
+            foreach (Rigidbody rb in m_ragdollRbs)
+                if (rb != m_mainRb)
+                    rb.isKinematic = true;
+            foreach (Collider col in m_ragdollColliders)
+                col.enabled = false;
+
+            //Activate main collider, main rigidbody and animator
+            m_mainCollider.enabled = true;
+            m_mainRb.isKinematic = false;
+            m_animator.enabled = true;
+        }
+    }
+}
