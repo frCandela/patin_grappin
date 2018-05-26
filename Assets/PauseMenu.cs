d652f52c2e04c98c57647d187e2ab3b00b6fdc60
Candela François <candela.francois@gmail.com>
2018-05-26 19:32:48 +0200

    Pause menu in game

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public UnityEvent onGameResumed;
+    public UnityEvent onGameQuit;
+
+    private void Awake()
+    {
+       // gameObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        onGameQuit.Invoke();
+    }
+
+    public void ResumeGame()
+    {
+        onGameResumed.Invoke();
+    }
+
+}
--- Assets/script/managers/LevelManager.cs
+++ Assets/script/managers/LevelManager.cs
@@ -7,25 +7,40 @@ public class LevelManager : MonoBehaviour
 {
     [Header("Parameters")]
     [SerializeField] private bool m_activateMusic = true;
+    [SerializeField] private string m_menuSceneString = "menu";
 
     //References
-    List<MonoBehaviour> m_pausedScripts;
+    private PauseMenu m_pauseMenu;
+    private List<MonoBehaviour> m_pausedScripts;
 
     //Private members
     private bool m_paused = false;
 
     private void Awake()
     {
+        m_pauseMenu = FindObjectOfType<PauseMenu>();
+        m_pauseMenu.gameObject.SetActive(false);
+
         m_pausedScripts = new List<MonoBehaviour>(FindObjectsOfType<boostEyeFX>());
         m_pausedScripts.Add(FindObjectOfType<PlayerController>());
     }
 
     void Start ()
     {
+        //Link events
+        m_pauseMenu.onGameQuit.AddListener(QuitToMenu);
+        m_pauseMenu.onGameResumed.AddListener(TooglePause);
+
         //Set music and sounds
         AkSoundEngine.PostEvent("Play_Speed_RTPC", gameObject);
         if (m_activateMusic)
             AkSoundEngine.PostEvent("Play_Music_Placeholder", gameObject);
+    }
+
+    private void QuitToMenu()
+    {
+        AkSoundEngine.PostEvent("Stop_Music_Placeholder", gameObject);
+        SceneManager.LoadScene(m_menuSceneString, LoadSceneMode.Single);
     }
 
     private void Update()
@@ -46,15 +61,18 @@ public class LevelManager : MonoBehaviour
     {
         m_paused = !m_paused;
 
+        //Pause the game
         if( m_paused)
         {
+            m_pauseMenu.gameObject.SetActive(true);
             Time.timeScale = 0f;
-
             foreach(MonoBehaviour m in m_pausedScripts)
                 m.enabled = false;
         }
+        //Resumes the game
         else
         {
+            m_pauseMenu.gameObject.SetActive(false);
             Time.timeScale = 1f;
             foreach (MonoBehaviour m in m_pausedScripts)
                 m.enabled = true;
