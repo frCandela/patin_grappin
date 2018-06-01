764ce572f3aafdb74a0fdbeb78c72aa5c5c7f350
Candela François <candela.francois@gmail.com>
2018-06-01 13:11:00 +0200

    First iteration of smooth respawn

--- Assets/script/track/Track.cs
+++ Assets/script/track/Track.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using System;
+using UnityEngine.Events;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class Track : MonoBehaviour
@@ -10,14 +12,21 @@ public class Track : MonoBehaviour
     //Editor parameters
     [Header("Track parameters: ")]
     [SerializeField] private float fallFromTrackHeight = 50;
-    [SerializeField] private float respawnHeight = 100f;
+    [SerializeField, Range(0, 1000f)] public float respawnHeight = 100f;
     [SerializeField] private float updateDelta = 0.2f;
     [SerializeField] private bool alignVelocityOnRespawn = false;
+
+    [Header("Boost: ")]
+    [SerializeField] private float boostHeight = 100f;
+    [SerializeField] private float boostDelta = 0.1f;
 
     [Header("Track Sections Tree: ")]
     [SerializeField] private TrackSection currentSection = null;
     [SerializeField] private bool checkPreviousSections = true;
     [SerializeField] private bool checkParallelSections = true;
+
+    [Header("Events")]
+    public UnityEvent onRespawn;
 
     //Private members
     private Rigidbody m_targetRb = null;
@@ -48,7 +57,7 @@ public class Track : MonoBehaviour
 
     IEnumerator UpdateTrack()
     {
-        while( true )
+        while (true)
         {
             //Use the closest track section
             float t1 = Time.realtimeSinceStartup;
@@ -56,44 +65,26 @@ public class Track : MonoBehaviour
             splineUpdateMs = 1000 * (Time.realtimeSinceStartup - t1);
             yield return new WaitForSeconds(updateDelta);
         }
-        
     }
 
     private void Update()
     {
-        //DetectPlayerFall();
+        float height =  m_targetRb.transform.position.y - currentSection.trackPosition.y;
+
+        if (height > boostHeight)
+            m_targetRb.transform.parent.position = m_targetRb.transform.parent.position - Time.deltaTime * boostDelta * Vector3.up;           
+
+        if (Input.GetKeyDown(KeyCode.T) || height < fallFromTrackHeight)
+            RespawnPlayer();
     }
 
-    private void DetectPlayerFall()
+    private void RespawnPlayer()
     {
-        if (currentSection != null && ! m_grap.grappling)
-        {
-            //Detects the player fall
-            Vector3 diff = currentSection.trackPosition - m_targetRb.transform.position;
-            if (diff.y > fallFromTrackHeight)
-            {
-                TrackSection respawnTrack = currentSection.respawnTrackSection ? currentSection.respawnTrackSection : currentSection;
+        //Translate the player
+        onRespawn.Invoke();
 
-                //If respawn set, change the respawn point
-                if (respawnTrack.respawnTransform)
-                    diff = currentSection.respawnTrackSection.respawnTransform.position - m_targetRb.transform.position;
-
-                //Translate the player
-                m_targetRb.transform.parent.transform.position = m_targetRb.transform.parent.transform.position + diff + respawnHeight * Vector3.up;
-
-                //Change track and update it
-                currentSection = respawnTrack;
-                respawnTrack.UpdateTrack(m_targetRb.transform.position);
-
-                //Changes his velocity to match the direction of the track
-                if (alignVelocityOnRespawn)
-                {
-                    float prevVelY = m_targetRb.velocity.y;
-                    float prevVelXZ = new Vector2(m_targetRb.velocity.x, m_targetRb.velocity.z).magnitude;
-                    m_targetRb.velocity = prevVelXZ * new Vector3(respawnTrack.trackDirection.x, 0, respawnTrack.trackDirection.z) + new Vector3(0, prevVelY, 0);
-                }
-            }
-        }
+        currentSection = currentSection.respawnTrackSection;
+        updateDelta = 1f;
     }
 
     public void UpdateClosestSection()
@@ -106,7 +97,7 @@ public class Track : MonoBehaviour
             Action<TrackSection> LookupSection = (section) =>
             {
                 section.UpdateTrack(m_targetRb.position);
-                float dist = Vector3.SqrMagnitude(m_targetRb.position - section.trackPosition);
+                float dist = Vector3.Magnitude(m_targetRb.position - section.trackPosition);
                 if (dist < bestDistance)
                 {
                     bestDistance = dist;
@@ -128,6 +119,8 @@ public class Track : MonoBehaviour
             //LookUp next sections
             foreach (TrackSection nextSection in currentSection.nextSections)
                 LookupSection(nextSection);
+            
+            LookupSection(currentSection);
 
             //Update new section
             if (bestTrackSection)
@@ -154,6 +147,9 @@ public class Track : MonoBehaviour
             style.normal.textColor = Color.red;
 
             GUI.Label(new Rect(0, 0, 100, 10), "Spline update ms: " + splineUpdateMs, style);
+
+            float height = m_targetRb.transform.position.y - currentSection.trackPosition.y;
+            GUI.Label(new Rect(0, 40, 100, 10), "Player height: " + height, style);
         }
     }
 }
--- Assets/script/track/TrackSpawner.cs
+++ Assets/script/track/TrackSpawner.cs
@@ -4,28 +4,103 @@ using UnityEngine;
 
 public class TrackSpawner : MonoBehaviour
 {
+    [SerializeField, Range(0,1000f)] private float respawnTrackDistance = 150f;
+    [SerializeField, Range(0, 10000)] private float respawnSpeedDelta = 3000;
+    [SerializeField] private float downOffset = 10000;
+    [SerializeField] private float lerpInValue = 0.1f;
+    [SerializeField] private float lerpOutValue = 0.1f;
 
-    private Track track;
     private Rigidbody m_targetRb = null;
 
+    private Track m_mainTrack;
+    private GameObject m_rescueTrack;
 
-    private GameObject rescueTrack;
+    private float respawnTime = 0f;
+
+    public State currentState = State.Following;
+
+    public enum State {Following, LerpingOut, LerpingIn};
 
     // Use this for initialization
     void Awake ()
     {
         m_targetRb = FindObjectOfType<PlayerController>().GetComponent<Rigidbody>();
-        track = FindObjectOfType<Track>();
+        m_mainTrack = FindObjectOfType<Track>();
+        m_mainTrack.onRespawn.AddListener(Respawn);
     }
 
     private void Start()
     {
-        rescueTrack = Instantiate(track.gameObject);
-        rescueTrack.transform.position = rescueTrack.transform.position - 100 * Vector3.up;
+        m_rescueTrack = Instantiate(m_mainTrack.gameObject);
+        Destroy(m_rescueTrack.GetComponent<Track>());
+
+        TagHierarchy(m_rescueTrack.transform);
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void TagHierarchy(Transform transform)
+    {
+        foreach (Transform trans in transform)
+        {
+            trans.gameObject.tag = "noGrab";
+            TagHierarchy(trans);
+        }
+            
+    }
+
+    void Respawn()
+    {
+        if (currentState == State.Following)
+        {
+            currentState = State.LerpingOut;
+
+            Vector3 diff = m_mainTrack.transform.position - m_targetRb.transform.parent.position;
+            m_targetRb.transform.parent.position = m_targetRb.transform.parent.position - (m_rescueTrack.transform.position - m_mainTrack.transform.position);
+
+            m_rescueTrack.transform.position = m_targetRb.transform.parent.position + diff;
+
+            respawnTime = Time.time;
+        }
+            
+
+       
+    }
+
+    private Vector3 GetRespawnTrackPosition()
+    {
+        Vector3 rescueTrackPos = m_targetRb.transform.position - m_mainTrack.trackSection.respawnTrackSection.respawnTransform.position;
+        rescueTrackPos.y = m_targetRb.transform.position.y - respawnTrackDistance - m_mainTrack.trackSection.respawnTrackSection.respawnTransform.position.y;
+        return rescueTrackPos;
+    }
+
+
+    private void FixedUpdate()   
+    {
+        switch (currentState)
+        {
+            case State.Following:
+                m_rescueTrack.transform.position = GetRespawnTrackPosition();
+                break;
+            case State.LerpingOut:
+                if (Time.time - respawnTime < 2)
+                    m_rescueTrack.transform.position = m_rescueTrack.transform.position + respawnSpeedDelta * Vector3.up;
+                else
+                {
+
+                    m_rescueTrack.transform.position = GetRespawnTrackPosition() - 10000 * Vector3.up;
+                    currentState = State.LerpingIn;
+                }
+                break;
+
+            case State.LerpingIn:
+                if (Time.time - respawnTime < 4)
+                    m_rescueTrack.transform.position = Vector3.Lerp(m_rescueTrack.transform.position, GetRespawnTrackPosition(), lerpInValue);                
+                else
+                {
+                    currentState = State.Following;
+                }
+                break;
+
+
+        }
+    }
 }
