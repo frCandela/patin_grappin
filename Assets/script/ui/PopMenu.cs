e6ec8c4db0c6ec211f6d6134ab06fdafcbcfd4af
Candela François <candela.francois@gmail.com>
2018-06-04 19:16:07 +0200

    khfg

---
+++ Assets/script/ui/PopMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopMenu : MonoBehaviour
+{
+    private LevelManager m_levelManager;
+    private bool m_active = false;
+
+    private void Awake()
+    {
+        m_levelManager = FindObjectOfType<LevelManager>();
+        Hide();
+    }
+
+    public void Pop(int id)
+    {
+        transform.GetChild(id).gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        foreach (Transform child in transform)
+            child.gameObject.SetActive(false);
+    }
+}
